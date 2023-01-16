a8a6393f191baf556dae6c5170c5b63354ac97da
jlab <jlab@172.17.153.203>
2023-01-16 18:34:13 +0900

    New analysis about Neural-Speed Correlation around and outside shock

--- Neural_analysis_speed.m
+++ Neural_analysis_speed.m
@@ -1,24 +1,59 @@
 clear all
 close all
 
-%% load neural data from doric recording and save as csv
-datapath = 'C:\Users\jlab\Documents\DoricData\';
-[file,path,indx] = uigetfile(fullfile(datapath,'*.csv'));
-fullname = [datapath,file];
+%% main path
+datapathDORIC = 'C:\Users\jlab\Documents\DoricData\';
+datapathBEH = 'C:\Users\jlab\Documents\BehData\';
+datapathTRACK = 'C:\Users\jlab\Documents\MATLAB\DannceProjects\';
+
+%% Load DORIC, BEH and TRACK data into memory
+% load neural data from doric recording and save as csv
+[file,path,indx] = uigetfile(fullfile(datapathDORIC,'*.csv'));
+fullname = fullfile(path,file);
+fprintf('Read %s data to memory \n',file)
 NeuralStructure = ReadDoric(fullname);
 
-%% load beh ttls from cheeta recording and save as csv
+% load beh ttls from cheeta recording and save as csv
+AnimalName = file(1:4);
+Date = char(datetime(file(9:16),'InputFormat','yyyyMMdd','Format','yyyy-MM-dd'));
+SelectTarget = string([Date,'*', AnimalName]);
+
 filename = 'Events.nev';
-[datapath] = uigetdir('C:\Users\jlab\Documents\BehData\');
+[datapath_temp] = dir(fullfile(datapathBEH,SelectTarget));
+if numel(datapath_temp) > 1
+    datapath_temp.name
+    prompt = "Which BEH file you want? \n";
+    x = input(prompt);
+    datapath = fullfile(datapath_temp(x).folder,datapath_temp(x).name);
+    names = datapath_temp(x).name;
+else
+    datapath = fullfile(datapath_temp.folder,datapath_temp.name);
+    names = datapath_temp.name;
+end
 fullname = [datapath,'\',filename];
+fprintf('Read %s data to memory \n',names)
 BehStrcture = read_nev(fullname);
 
-%% extract speed from video
+% extract speed from video
+Date = char(datetime(file(9:16),'InputFormat','yyyyMMdd','Format','yyyy_MM_dd'));
+SelectTarget = string([AnimalName,'_',Date,'*']);
+
 filename = 'COM\predict_results\com3d.mat';
-[datapath] = uigetdir('C:\Users\jlab\Documents\MATLAB\DannceProjects\');
+[datapath_temp] = dir(fullfile(datapathTRACK,SelectTarget));
+if numel(datapath_temp) > 1
+    datapath_temp.name
+    prompt = "Which TRACK file you want? \n";
+    x = input(prompt);
+    datapath = fullfile(datapath_temp(x).folder,datapath_temp(x).name);
+    names = datapath_temp(x).name;
+else
+    datapath = fullfile(datapath_temp.folder,datapath_temp.name);
+    names = datapath_temp.name;
+end
 fullname = [datapath,'\',filename];
-coordi3D = load(fullname);
-Com = coordi3D.com;
+fprintf('Read %s data to memory \n',names)
+TrackStrcture = load(fullname);
+Com = TrackStrcture.com;
 FS_cam = 20;
 
 %% interpolate beh timestamps to neural time space
@@ -118,18 +153,18 @@ plot(NeuralTime,ComIND*100)
 Com_speed = sqrt((Com_denoised(2:end,1)-Com_denoised(1:end-1,1)).^2 + (Com_denoised(2:end,2)-Com_denoised(1:end-1,2)).^2 + (Com_denoised(2:end,3)-Com_denoised(1:end-1,3)).^2);
 Com_speed = Com_speed./diff(NeuralTime);
 Com_speed = [Com_speed(1,1);Com_speed];
-
+Z_Com_speed = zscore(Com_speed);
 %%
 Delat_Chan_GCamP_highpass = MotionCorrected_GCamP./Chan_GCamP_Baseline;
 Z_Delat_Chan_GCamP_highpass = zscore(Delat_Chan_GCamP_highpass);
 
 figure; hold on;
 plot(NeuralTime,Z_Delat_Chan_GCamP_highpass)
-plot(NeuralTime,zscore(Com_speed))
+plot(NeuralTime,Z_Com_speed)
 
 %% correlate spedd and neural signal
 A = Z_Delat_Chan_GCamP_highpass';
-B = zscore(Com_speed)';
+B = Z_Com_speed';
 
 figure;
 [xcf,lags,~,h] = crosscorr(A,B,NumLags=1200);
@@ -141,7 +176,7 @@ LagInd2 = (1:length(A)-lag);
 
 A = Z_Delat_Chan_GCamP_highpass(LagInd1,1)'; %% latter
 B = zscore(Com_speed(LagInd2,1))'; %% sonner
- 
+
 figure; hold on;
 scatter(A(1:1200:end), B(1:1200:end));
 [rcof,pval]=corrcoef(A,B);
@@ -154,7 +189,7 @@ ylabel('Speed')
 
 %% thresholding speed
 A = Z_Delat_Chan_GCamP_highpass';
-B = zscore(Com_speed)';
+B = Z_Com_speed';
 
 A(B<0) = [];
 B(B<0) = [];
@@ -174,7 +209,7 @@ Delat_Chan_GCamP_highpass = MotionCorrected_GCamP./Chan_GCamP_Baseline;
 
 figure; hold on;
 Z_Delat_Chan_GCamP_highpass = zscore(Delat_Chan_GCamP_highpass);
-Z_Com_speed = zscore(Com_speed);
+Z_Com_speed = Z_Com_speed;
 
 plot(NeuralTime,Z_Delat_Chan_GCamP_highpass);
 plot(NeuralTime,Z_Com_speed);
@@ -208,9 +243,24 @@ title('Normalized \Delta F/F for Recording')
 
 
 %% calculate speed around shock
-Z_Com_speed = zscore(Com_speed);
+Z_Com_speed = Z_Com_speed;
 Ch490 = MotionCorrected_GCamP;
 Ch405 = Chan_GCamP_Baseline; % (-inf ~ 0.001)
+
+EventName = {'Shock','Sound1','WP1','WP2','IR1ON','IR2ON'};
+EventsON = {};
+EventsOFF = {};
+names = fieldnames(DataStrcture);
+for i = 1:length(names)
+    for k = 1:length(EventName)
+        if and(~isempty(strfind(names{i},EventName{k})), ~isempty(strfind(names{i},'ON')))
+            ONtime = DataStrcture.(names{i});
+            OFFtime = DataStrcture.([names{i}(1:end-2),'OFF']);
+            EventsON{k} = [ONtime];
+            EventsOFF{k} = [OFFtime];
+        end
+    end
+end
 
 NonOverlapping = 1;
 SpeacialTime = 0;
@@ -229,23 +279,8 @@ PostWind=round(Post*FS);
 
 bin=FS/5; %FS/10
 
-Ts = NeuralTime;
-
 % Get event TS
 EventID = 1;
-EventName = {'Shock','Sound1','WP1','WP2','IR1ON','IR2ON'};
-EventsON = {};
-names = fieldnames(DataStrcture);
-for i = 1:length(names)
-    for k = 1:length(EventName)
-        if and(~isempty(strfind(names{i},EventName{k})), ~isempty(strfind(names{i},'ON')))
-            ONtime = DataStrcture.(names{i});
-            OFFtime = DataStrcture.([names{i}(1:end-2),'OFF']);
-            EventsON{k} = [ONtime];
-        end
-    end
-end
-
 EventTS = EventsON{EventID};
 
 if SpeacialTime == 1
@@ -256,9 +291,9 @@ if SpeacialTime == 1
     RwardTime1 = EventsON{3}(find(diff([0;EventsON{3}])>30));
     RwardTime2 = EventsON{4}(find(diff([0;EventsON{4}])>30));
     EventTS = sort([RwardTime1';RwardTime2']);
-%     [~,RewLick1] = sparse_distanceXY(RwardTime1,EventsON{5});
-%     [~,RewLick2] = sparse_distanceXY(RwardTime2,EventsON{6});
-%     EventTS = sort([RewLick1';RewLick2']);
+    %     [~,RewLick1] = sparse_distanceXY(RwardTime1,EventsON{5});
+    %     [~,RewLick2] = sparse_distanceXY(RwardTime2,EventsON{6});
+    %     EventTS = sort([RewLick1';RewLick2']);
 end
 
 % Remove overlapping timestamps
@@ -278,7 +313,7 @@ end
 
 CSidx=[];
 for i=1:length(EventTS)
-    [~, CSidx(i,1)]=min(abs(Ts(:,:)-EventTS(i)));
+    [~, CSidx(i,1)]=min(abs(NeuralTime(:,:)-EventTS(i)));
 end
 
 % Obtain the DeltaF/F for each event window
@@ -287,7 +322,7 @@ counter=1;
 clear DF_Event DF_F DF_Base DF_ZScore F490CSBL F405CSBL F405 F490 bls Y_Fit ZSpeed SP_ZScore
 for i=1:length(CSidx)
     % Check CS events are not earlier than session start and not latter than session end
-    if CSidx(i)-(BaselineWind+BaselineWind2)<=0 || CSidx(i)+PostWind > length(Ts)
+    if CSidx(i)-(BaselineWind+BaselineWind2)<=0 || CSidx(i)+PostWind > length(NeuralTime)
     else
         % Baseline Signal
         F490CSBL(1,:)=Ch490((CSidx(i)-(BaselineWind+BaselineWind2)):(CSidx(i)-(BaselineWind)));
@@ -333,7 +368,7 @@ SPTrace1SEM = (std(SP_ZScore_bin,1)./sqrt(size(SP_ZScore_bin,1)));
 
 % Plotting PSTH
 % Z-value (subtract baseline)
-figure; 
+figure;
 subplot(2,1,1); hold on;
 shadedErrorBar(CSTS_bin,CSTrace1,CSTrace1SEM,{'-b','color',[0,0,0]},0.5)
 plot(CSTS_bin,CSTrace1,'-k','LineWidth',2)
@@ -372,17 +407,99 @@ subplot(2,1,2);
 imagesc(CSTS_bin,[1:size(SP_ZScore_bin,1)],SP_ZScore_bin,[-3,3]);
 ylabel('Trial #','fontsize', 18)
 title([EventName{EventID},' Z-score PSTH'],'fontsize', 18)
-xlim([-Pre Post
+xlim([-Pre Post])
 set(gca,'fontsize',18);
 
 %% Trace with neural activity as color
 Delat_Chan_GCamP_highpass = MotionCorrected_GCamP./Chan_GCamP_Baseline;
 Z_Delat_Chan_GCamP_highpass = zscore(Delat_Chan_GCamP_highpass);
-Z_Com_speed = zscore(Com_speed);
+Z_Com_speed = Z_Com_speed;
 
 figure; scatter3(Com_denoised(:,1),Com_denoised(:,2),Com_denoised(:,3),1,MotionCorrected_GCamP)
 figure; scatter3(Com_denoised(:,1),Com_denoised(:,2),Com_denoised(:,3),1,Com_speed)
 
 %%  shock-speed Correlation within shock [0,25]
+% Get event TS % EventName = {'Shock','Sound1','WP1','WP2','IR1ON','IR2ON'};
+EventID = 1;
+EventTS = EventsON{EventID};
 
+CSidx=[];
+for i=1:length(EventTS)
+    [~, CSidx(i,1)]=min(abs(NeuralTime(:,:)-EventTS(i)));
+end
+TimeIndex = unique(bsxfun(@plus, CSidx, [0:25*FS]));
+OutTimeIndex = setdiff(1:length(NeuralTime),TimeIndex);
 
+% correlate spedd and neural signal around timestamp
+A = Z_Delat_Chan_GCamP_highpass(TimeIndex)';
+B = zscore(Com_speed(TimeIndex))';
+
+% thresholding speed
+A(B<0) = [];
+B(B<0) = [];
+
+figure; hold on;
+scatter(A(1:1:end), B(1:1:end));
+[rcof,pval]=corrcoef(A,B);
+text(0.2,0.9,['R = ',num2str(round(rcof(1,2),3))],'units','normalized','FontSize',12,'HorizontalAlignment','left','FontWeight','bold');
+text(0.2,0.9-0.08,['p < 10^','{',num2str(ceil(log10(pval(2,1)))),'}'],'units','normalized','FontSize',12,'HorizontalAlignment','left','FontWeight','bold')
+C=regress(B',[repmat(1,numel(A),1),A']);
+plot(linspace(min(A),max(A),11),linspace(min(A),max(A),11).*C(2)+C(1),'--','linewidth',3);
+title("Speed-Neural Correlation around stimulus")
+xlabel('Neural')
+ylabel('Speed')
+
+%% binning speed and neural signal
+% Z_Delat_Chan_GCamP_highpass
+bins = [min(Z_Com_speed)-1:1:max(Z_Com_speed)+1];
+DisIndSpeed = discretize(Z_Com_speed,bins);
+discretedSpeed = bins(DisIndSpeed);
+
+clear AvgSpeed SEMSpeed
+for i = 1:length(bins)
+    AvgSpeed(i) = mean(Z_Delat_Chan_GCamP_highpass(find(DisIndSpeed==i)));
+    SEMSpeed(i) = std(Z_Delat_Chan_GCamP_highpass(find(DisIndSpeed==i)))./sqrt(numel(find(DisIndSpeed==i)));
+end
+
+figure; hold on;
+shadedErrorBar(bins,AvgSpeed,SEMSpeed,{'-b','color',[0,0,0]},0.5)
+plot(bins,AvgSpeed,'-k','LineWidth',2)
+title("Binning Speed see Neural")
+xlabel('Speed')
+ylabel('Neural')
+
+% binning around timestamp
+bins = [min(Z_Com_speed)-1:1:max(Z_Com_speed)+1];
+DisIndSpeed = discretize(Z_Com_speed(TimeIndex),bins);
+discretedSpeed = bins(DisIndSpeed);
+
+clear AvgSpeed SEMSpeed
+for i = 1:length(bins)
+    AvgSpeed(i) = mean(Z_Delat_Chan_GCamP_highpass(find(DisIndSpeed==i)));
+    SEMSpeed(i) = std(Z_Delat_Chan_GCamP_highpass(find(DisIndSpeed==i)))./sqrt(numel(find(DisIndSpeed==i)));
+end
+
+figure; hold on;
+shadedErrorBar(bins,AvgSpeed,SEMSpeed,{'-b','color',[0,0,0]},0.5)
+plot(bins,AvgSpeed,'-k','LineWidth',2)
+title("Binning Speed see Neural - around timestamp")
+xlabel('Speed')
+ylabel('Neural')
+
+% binning outside timestamp
+bins = [min(Z_Com_speed)-1:1:max(Z_Com_speed)+1];
+DisIndSpeed = discretize(Z_Com_speed(OutTimeIndex),bins);
+discretedSpeed = bins(DisIndSpeed);
+
+clear AvgSpeed SEMSpeed
+for i = 1:length(bins)
+    AvgSpeed(i) = mean(Z_Delat_Chan_GCamP_highpass(find(DisIndSpeed==i)));
+    SEMSpeed(i) = std(Z_Delat_Chan_GCamP_highpass(find(DisIndSpeed==i)))./sqrt(numel(find(DisIndSpeed==i)));
+end
+
+figure; hold on;
+shadedErrorBar(bins,AvgSpeed,SEMSpeed,{'-b','color',[0,0,0]},0.5)
+plot(bins,AvgSpeed,'-k','LineWidth',2)
+title("Binning Speed see Neural - outside timestamp")
+xlabel('Speed')
+ylabel('Neural')
